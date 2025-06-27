bd5f98b5f94e5fdd26ab0dfc99cd475992dc677e
KiloBites <kiloktane@gmail.com>
2025-06-26 22:11:02 -0400

    All 24 possible corners can now be checked (hopefully)

--- Assets/ModScripts/PaintingCubeScript.cs
+++ Assets/ModScripts/PaintingCubeScript.cs
@@ -65,19 +65,42 @@ public class PaintingCubeScript : MonoBehaviour
         new[] { 2, 1, 5, 3, 0, 4 }
     };
 
-    private static readonly int[][] corners =
-    {
-        Enumerable.Range(0, 3).ToArray(),
+
+    private static readonly int[][] corners = 
+        {
+        new[] { 0, 1, 2 },
+        new[] { 1, 2, 0 },
+        new[] { 2, 0, 1 },
+
         new[] { 0, 2, 3 },
-        new[] { 0, 3, 4 },
-        new[] { 0, 4, 1 },
-        new[] { 3, 4, 0 },
+        new[] { 2, 3, 0 },
         new[] { 3, 0, 2 },
+
+        new[] { 0, 3, 4 },
+        new[] { 3, 4, 0 },
+        new[] { 4, 0, 3 },
+
+        new[] { 0, 4, 1 },
+        new[] { 4, 1, 0 },
+        new[] { 1, 0, 4 },
+
+        new[] { 5, 2, 1 },
+        new[] { 2, 1, 5 },
         new[] { 1, 5, 2 },
+
+        new[] { 5, 3, 2 },
+        new[] { 3, 2, 5 },
         new[] { 2, 5, 3 },
+
+        new[] { 5, 4, 3 },
+        new[] { 4, 3, 5 },
         new[] { 3, 5, 4 },
+
+        new[] { 5, 1, 4 },
+        new[] { 1, 4, 5 },
         new[] { 4, 5, 1 }
     };
+
 
     private Vector3 ObtainGridPos(int cubePos)
     {
